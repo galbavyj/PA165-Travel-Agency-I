6b80ee0e75e9eb512676df048af9d788e0508e61
galbavyj <diablo112@centrum.sk>
2016-10-26 23:38:09 +0200

    Reservation Class, DAO update (#5)
    * Reservation Update + empty entities
    
    * Reservation class update
    
    Fix
    
    * ReservationDaoImpl
    
    ReservationDaoImpl implemented
    
    * admin and customer classes removed
    
    admin and customer classes removed

--- TravelAgency-Persistence/src/main/java/cz/muni/fi/pa165/travelAgency/persistence/dao/ReservationDaoImpl.java
+++ TravelAgency-Persistence/src/main/java/cz/muni/fi/pa165/travelAgency/persistence/dao/ReservationDaoImpl.java
@@ -1,9 +1,61 @@
 package cz.muni.fi.pa165.travelAgency.persistence.dao;
 
+import cz.muni.fi.pa165.travelAgency.persistence.entity.Reservation;
+import cz.muni.fi.pa165.travelAgency.persistence.entity.Trip;
+import cz.muni.fi.pa165.travelAgency.persistence.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 /**
  * @author Juraj Galbavý
  */
-public class ReservationDaoImpl {
+public class ReservationDaoImpl implements ReservationDao{
 
-    //TODO
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Reservation reservation) {
+        em.persist(reservation);
+    }
+
+    @Override
+    public Reservation update(Reservation reservation) {
+        return em.merge(reservation);
+    }
+
+
+    @Override
+    public void remove(Reservation reservation) {
+        em.remove(findById(reservation.getId()));
+    }
+
+    @Override
+    public List<Reservation> findAllReservations() {
+        return em.createQuery("SELECT r FROM Reservation r", Reservation.class)
+                .getResultList();
+    }
+
+    @Override
+    public List<Reservation> findReservationsByUser(User user) {
+        return em.createQuery("SELECT r FROM Reservation r WHERE r.user=:user", Reservation.class)
+                .setParameter("user", user).getResultList();
+
+    }
+
+    @Override
+    public List<Reservation> findReservationsByTrip(Trip trip) {
+        return em.createQuery("SELECT r FROM Reservation r WHERE r.trip=:trip", Reservation.class)
+                .setParameter("trip", trip).getResultList();
+
+    }
+
+    @Override
+    public Reservation findById(Long id) {
+        return em.find(Reservation.class, id);
+    }
+
+
 }
