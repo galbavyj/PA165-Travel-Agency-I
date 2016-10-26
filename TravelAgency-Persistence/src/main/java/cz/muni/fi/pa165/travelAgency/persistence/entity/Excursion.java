4ef1531d027447e12fdbfa2d88c1bf5e9020262e
blahutova <bahutova.lucia@gmail.com>
2016-10-26 19:26:14 +0200

    repaired excursion

--- TravelAgency-Persistence/src/main/java/cz/muni/fi/pa165/travelAgency/persistence/entity/Excursion.java
+++ TravelAgency-Persistence/src/main/java/cz/muni/fi/pa165/travelAgency/persistence/entity/Excursion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -126,13 +125,4 @@ public class Excursion {
     public Excursion(){
         
     }
-=======
-package cz.muni.fi.pa165.travelAgency.persistence.entity;
-
-/**
- * //TODO
- */
-public class Excursion {
-    //TODO
->>>>>>> origin/master
 }
