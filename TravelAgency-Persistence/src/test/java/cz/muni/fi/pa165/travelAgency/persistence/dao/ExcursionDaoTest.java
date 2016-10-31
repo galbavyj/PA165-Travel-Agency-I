e25a3f8e2b82d3e464ab8af51fe37a6e115f7e7e
galbavyj <diablo112@centrum.sk>
2016-10-31 23:26:46 +0100

    ExcursionTest

--- TravelAgency-Persistence/src/test/java/cz/muni/fi/pa165/travelAgency/persistence/dao/ExcursionDaoTest.java
+++ TravelAgency-Persistence/src/test/java/cz/muni/fi/pa165/travelAgency/persistence/dao/ExcursionDaoTest.java
@@ -53,6 +53,7 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
     private Excursion excursion2;
 
     private Trip trip1;
+    private Address address;
 
 
 
@@ -69,7 +70,7 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         calendar.set(2017, Calendar.APRIL, 10, 8, 0);
         Date startExcursion1 = calendar.getTime();
 
-        calendar.set(2017, Calendar.APRIL, 19, 8, 0);
+        calendar.set(2017, Calendar.APRIL, 15, 8, 0);
         Date startExcursion2 = calendar.getTime();
 
         calendar.set(2017, Calendar.APRIL, 1, 8, 0);
@@ -82,8 +83,6 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         excursions.add(excursion1);
         excursions.add(excursion2);
 
-        trip1 = new Trip();
-
 
 
         excursion1 = new Excursion();
@@ -91,11 +90,10 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         excursion1.setDescription("Good enough");
         excursion1.setDurationInHours(10);
         excursion1.setFromDate(startExcursion1);
-        excursion1.setPlace("London");
+        excursion1.setPlace("Greenwich");
         excursion1.setPrice(BigDecimal.valueOf(1500));
-        //excursion1.setTrip(trip1);
+        excursion1.setTrip(trip1);
         excursion1.setExcursionType(ExcursionType.CULTURE);
-
 
 
         excursion2 = new Excursion();
@@ -105,42 +103,48 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         excursion2.setFromDate(startExcursion2);
         excursion2.setPlace("Wien");
         excursion2.setPrice(BigDecimal.valueOf(2000));
-        //excursion2.setTrip(trip1);
+        excursion2.setTrip(trip1);
         excursion2.setExcursionType(ExcursionType.ENTERTAINMENT);
 
+        trip1 = new Trip();
+        address = new Address();
+        address.setCity("London");
+        address.setCountry("UK");
+        address.setNumberOfHouse(15);
+        address.setStreet("George Street");
         trip1.setCreated(created1);
         trip1.setFrom(startTrip);
         trip1.setTo(endTrip);
         trip1.setPossibleExcursions(excursions);
         trip1.setPrice(BigDecimal.valueOf(15000));
-        trip1.setAddressOfHotel(new Address());
+        trip1.setAddressOfHotel(address);
 
     }
 
 
     @Test
-    public void create(){
+    public void excursionCreate(){
         excursionDao.create(excursion1);
         assertTrue(excursionDao.findAllExcursions().contains(excursion1));
         assertTrue(excursionDao.findAllExcursions().size() == 1);
     }
 
     @Test
-    public void remove(){
+    public void eUpdate(){
         excursionDao.create(excursion1);
-        excursionDao.remove(excursion1);
-        assertEquals(excursionDao.findAllExcursions().size(), 0);
+        assertEquals(excursionDao.findExcursionById(excursion1.getId()).getPlace(), "Greenwich");
+        excursion1.setPlace("BigBen");
+        excursionDao.update(excursion1);
+        assertEquals(excursionDao.findExcursionById(excursion1.getId()).getPlace(), "BigBen");
+        assertFullEquals(excursionDao.findExcursionById(excursion1.getId()), excursion1);
     }
 
 
     @Test
-    public void update(){
+    public void remove(){
         excursionDao.create(excursion1);
-        assertEquals(excursionDao.findExcursionById(excursion1.getId()).getPlace(), "London");
-        excursion1.setPlace("Amsterdam");
-        excursionDao.update(excursion1);
-        assertEquals(excursionDao.findExcursionById(excursion1.getId()).getPlace(), "Amsterdam");
-        assertFullEquals(excursionDao.findExcursionById(excursion1.getId()), excursion1);
+        excursionDao.remove(excursion1);
+        assertEquals(excursionDao.findAllExcursions().size(), 0);
     }
 
     @Test
@@ -158,21 +162,6 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         assertTrue(excursionDao.findAllExcursions().size() == 2);
     }
 
-    /*@Test
-    public void findAllExcursionsInReservation() throws Exception {
-
-    }*/
-
-    /*@Test
-    public void excursionInTrip(){
-        excursionDao.create(excursion1);
-        tripDao.create(trip1);
-        assertTrue(excursionDao.findExcursionById(excursion1.getId()));
-
-    }*/
-
-
-
     @Test(expectedExceptions = ConstraintViolationException.class)
     public void testCreateWithNullPlace() {
         excursion1.setPlace(null);
@@ -185,11 +174,11 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         excursionDao.create(excursion1);
     }
 
-    /*@Test(expectedExceptions = InvalidArgumentException.class)
-    public void testCreateWithZeroTripDuration() {
-        excursion1.setDurationInHours(-1);
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testCreateWithNegativePrice() {
+        excursion1.setPrice(BigDecimal.valueOf(-1));
         excursionDao.create(excursion1);
-    }*/
+    }
 
     @Test(expectedExceptions = ConstraintViolationException.class)
     public void testCreateWithNullDateFrom() {
@@ -203,7 +192,6 @@ public class ExcursionDaoTest extends AbstractTestNGSpringContextTests{
         excursion1.setExcursionType(null);
         excursionDao.create(excursion1);
     }
-
 
     private void assertFullEquals(Excursion excursion, Excursion excursion1) {
         assertEquals(excursion, excursion1);
