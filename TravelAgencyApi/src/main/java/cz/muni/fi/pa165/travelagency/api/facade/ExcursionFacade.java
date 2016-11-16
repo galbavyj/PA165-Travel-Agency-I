38f7dbaf774cf32dad900166c969bb41a5da1479
blahutova <bahutova.lucia@gmail.com>
2016-11-16 19:43:21 +0100

    implemented facade layer and update function for service and facade

---
+++ TravelAgencyApi/src/main/java/cz/muni/fi/pa165/travelagency/api/facade/ExcursionFacade.java
@@ -0,0 +1,64 @@
+package cz.muni.fi.pa165.travelagency.api.facade;
+
+import cz.muni.fi.pa165.travelagency.api.dto.ExcursionDTO;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ * @author Lucia
+ */
+public interface ExcursionFacade {
+    /**
+     * Create new excursion
+     *
+     * @param excursion DTO of excursion, which you want to create
+     * @return ID of new excursion
+     */
+    public Long createExcursion(ExcursionDTO excursion);
+
+    /**
+     * Delete the excursion
+     *
+     * @param ex DTO of excursion, which will be deleted
+     */
+    public void deleteExcursion(ExcursionDTO ex);
+
+    /**
+     * Change the description of the excursion
+     *
+     * @param excursion excursion which will be changed
+     * @param description new description of excursion
+     */
+    public void changeDescription(ExcursionDTO excursion, String description);
+    
+    /**
+     * Change the description of the excursion
+     *
+     * @param excursion DTO of excursion which will be changed
+     * @param price new price of excursion
+     */
+    public void changePrice(ExcursionDTO excursion, BigDecimal price);
+
+    
+    /**
+     * Find all excursions
+     *
+     * @return all excursions
+     */
+    public List<ExcursionDTO> findAllExcursions();
+    
+    /**
+     * Find some excursion by its ID
+     * @param exId ID of excursion
+     * @return found excursion
+     */
+    public ExcursionDTO findExcursionById(Long exId); 
+    
+    /**
+     * Update excursion
+     * 
+     * @param ex DTO of excursion to be updated
+     */
+    void updateExcursion(ExcursionDTO ex);
+}
---
+++ TravelAgencyService/src/main/java/facade/ExcursionFacadeImpl.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import cz.muni.fi.pa165.travelAgency.persistence.entity.Excursion;
+import cz.muni.fi.pa165.travelagency.api.dto.ExcursionDTO;
+import cz.muni.fi.pa165.travelagency.api.facade.ExcursionFacade;
+import cz.muni.fi.pa165.travelagency.travelagencyservice.MappingService;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.inject.Inject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import service.ExcursionService;
+
+/**
+ *
+ * @author Lucia
+ */
+@Service
+@Transactional
+public class ExcursionFacadeImpl implements ExcursionFacade {
+    
+    @Autowired
+    private MappingService mappingService;
+    
+    @Inject
+    private ExcursionService excursionService;
+
+    @Override
+    public Long createExcursion(ExcursionDTO excursion) {
+        Excursion mappedExcursion = mappingService.mapTo(excursion, Excursion.class);
+        Excursion newExcursion = excursionService.createExcursion(mappedExcursion);
+        return newExcursion.getId();
+    }
+
+    @Override
+    public void deleteExcursion(ExcursionDTO ex) {
+        Excursion mappedExcursion = mappingService.mapTo(ex, Excursion.class);
+        excursionService.deleteExcursion(mappedExcursion);
+    }
+
+    @Override
+    public void changeDescription(ExcursionDTO excursion, String description) {
+        Excursion mappedExcursion = mappingService.mapTo(excursion, Excursion.class);
+        excursion.setDescription(description);
+        excursionService.updateExcursion(mappedExcursion);
+    }
+
+    @Override
+    public void changePrice(ExcursionDTO excursion, BigDecimal price) {
+        Excursion mappedExcursion = mappingService.mapTo(excursion, Excursion.class);
+        excursion.setPrice(price);
+        excursionService.updateExcursion(mappedExcursion);
+    }
+
+    @Override
+    public List<ExcursionDTO> findAllExcursions() {
+        return mappingService.mapTo(excursionService.findAllExcursions(), ExcursionDTO.class);
+    }
+
+    @Override
+    public ExcursionDTO findExcursionById(Long exId) {
+        return mappingService.mapTo(excursionService.findExcursionById(exId), ExcursionDTO.class);
+
+    }
+
+    @Override
+    public void updateExcursion(ExcursionDTO ex) {
+        Excursion mappedExcursion = mappingService.mapTo(ex, Excursion.class);
+        excursionService.updateExcursion(mappedExcursion);
+    }
+    
+}
